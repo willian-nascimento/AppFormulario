6e5629a4bd3d9a3b1de66ba33ce886294b0249a8
elbertribeiro <elbertprofissional@gmail.com>
2022-06-29 20:40:02 -0300

    criacao de testes e refatoracao

--- formulario-api/src/main/java/com/github/formularioapi/core/Formulario.java
+++ formulario-api/src/main/java/com/github/formularioapi/core/Formulario.java
@@ -1,10 +1,25 @@
 package com.github.formularioapi.core;
 
-import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Data @Document public class Formulario {
+@Document public class Formulario {
     @Id private String id;
     private String nameUser;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNameUser() {
+        return nameUser;
+    }
+
+    public void setNameUser(String nameUser) {
+        this.nameUser = nameUser;
+    }
 }
