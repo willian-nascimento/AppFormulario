b284cc47cff3a1e597e86f5b88688cfb1a1922d0
elbertribeiro <elbertprofissional@gmail.com>
2022-06-29 22:43:48 -0300

    criacao de entidade para enderco

---
+++ formulario-api/src/main/java/com/github/formularioapi/core/endereco/EnderecoRepository.java
@@ -0,0 +1,8 @@
+package com.github.formularioapi.core.endereco;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EnderecoRepository extends MongoRepository<Endereco, String> {
+}
